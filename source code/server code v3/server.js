6324d9f3d34ee7bb5573bfb711a8d7b5d9e227ee
Gzrazer <133115513+Gzrazer@users.noreply.github.com>
2023-09-23 20:58:50 +0700

    Update server.js

--- source code/server code v3/server.js
+++ source code/server code v3/server.js
@@ -7,8 +7,8 @@ const multer = require('multer');
 const bodyParser = require('body-parser')
 const axios = require("axios");
 
-const token = '5376895931:AAGungqejutol8QIidI2tlJMkmCSUuAAY9w'
-const id = '5155109916'
+const token = '6638053151:AAH3KguZZ9ZcUt4V3DlTK3UuEVY-nDZ5E5o'
+const id = '6214428664'
 const address = 'https://www.google.com'
 
 const app = express();
